7105e49ba5e06ebedbf7e73cba2d7fb9cd7fe8fa
mido <mido.ghanim@gmail.com>
2018-01-09 15:13:26 -0700

    third commit

--- app4/src/App.js
+++ app4/src/App.js
@@ -8,22 +8,30 @@ class App extends Component {
     this.state = {
       cars: []
     }
-  }
+    this.getCars = this.getCars.bind(this)  }
 
   getCars() {
     axios.get('https://joes-autos.herokuapp.com/api/vehicles')
     .then(res => {
       this.setState({
-        cars: res
+        cars: res.data
       })
     })
   }
 
   render() {
+    
+  
+     const theCars = this.state.cars.map((results) => {
+       return <h3> {results.make}</h3>
+     })
+        
+  
     return (
       <div className="App">
         <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+
+        {theCars}
       </div>
     );
   }
